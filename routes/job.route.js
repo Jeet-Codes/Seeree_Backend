276696df2df8e513c5273e96a9025b0453d7b269
Sandeep <sandeepkumar090304@gmail.com>
2024-09-02 08:59:35 +0530

    Update 1.4

--- routes/job.route.js
+++ routes/job.route.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.route("/post").post(isAuthenticated, postJob);
 router.route("/get").get(isAuthenticated, getAllJobs);
 router.route("/getadminjobs").get(isAuthenticated, getAdminJobs);
-router.route("/get/:id").get(isAuthenticated, getJobById);
+// router.route("/get/:id").get(isAuthenticated, getJobById);
+router.route("/get/:id").get(getJobById);
 router.route('/jobs/:id').delete(deleteJob)
 export default router;
 
