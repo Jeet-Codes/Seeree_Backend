19cfcc80dc0632289f2889396a24fd9df890982c
Bikash Malu <141820540+Bikash-Malu@users.noreply.github.com>
2024-11-13 18:54:31 +0530

    add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://seereejobportal.vercel.app',
     credentials:true
 }
 
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000;
 // api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
-app.use("/api/v1/job", jobRoute)
+app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 
